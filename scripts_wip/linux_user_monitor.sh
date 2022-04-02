254a6137d043c3c880255e67fd5a0f5049b78f14
silversword411 <github.com@datmail.com>
2022-04-02 13:03:43 -0400

    wip linux new user monitor

---
+++ scripts_wip/linux_user_monitor.sh
@@ -0,0 +1,21 @@
+Here's monitor for users on Linux:
+
+#!/bin/bash
+
+new="/opt/rmmscripts/users_new"
+old="/opt/rmmscripts/users_old"
+
+if [[ ! -e $old ]]; then
+mkdir /opt/rmmscripts
+cat /etc/passwd > $new
+fi
+
+mv $new $old
+cat /etc/passwd > $new
+diff <(cat $old) <(cat $new)
+if [[ $? == 0 ]] ; then 
+    echo "no users added or deleted"
+else
+    echo "user(s) added or deleted"
+    exit 1
+fi
