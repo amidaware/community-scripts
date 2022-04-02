acb00f635d2817d31653ea71a0eb02b1cbcc6f98
dinger1986 <daniel.lamb@flonix.co.uk>
2022-04-02 23:20:31 +0100

    Update linux_disk_check.sh

--- scripts_wip/linux_disk_check.sh
+++ scripts_wip/linux_disk_check.sh
@@ -8,9 +8,9 @@ for disk in $output; do
     partition=$(echo "${disk}" | awk '{ print $2 }')
     if [ ${usep} -ge 90 ]; then
         echo "Running out of space $partition ${usep}%"
+        Exit 1
     else
         echo "${partition} Disk space is fine at ${usep}%"
+        exit 0
     fi
 done
-
-output=$(df -h | grep -vE '^Filesystem|tmpfs|cdrom|udev' | awk '{ print $5 " " $1 }')
