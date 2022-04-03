82620eae0fbc4a9d590e7bf8830afc3a2c709c13
dinger1986 <daniel.lamb@flonix.co.uk>
2022-04-03 14:31:22 +0100

    Update Win_Driver_Restrict_PrinterInstallToAdmin.bat

--- scripts_wip/Win_Driver_Restrict_PrinterInstallToAdmin.bat
+++ scripts_wip/Win_Driver_Restrict_PrinterInstallToAdmin.bat
@@ -1,1 +1,3 @@
+REM This Script will restrict adding printers to Admins only
+
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v RestrictDriverInstallationToAdministrators /t REG_DWORD /d 1 /f
