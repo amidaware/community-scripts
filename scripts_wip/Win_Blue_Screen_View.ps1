5082ab50580a366a665f29d2b8def3b79a333b8a
silversword411 <github.com@datmail.com>
2022-04-03 17:52:56 -0400

    more renames

--- scripts_wip/Win_Blue_Screen_View.ps1
+++ scripts_wip/Win_Blue_Screen_View.ps1
@@ -3,32 +3,33 @@ If (!(test-path "c:\temp")) {
     New-Item -ItemType Directory -Force -Path "c:\temp"
 }
 
-If(!(test-path $env:programdata\RMMScripts\))
-{
-      New-Item -ItemType Directory -Force -Path $env:programdata\TRMMScripts\
+If (!(test-path $env:programdata\RMMScripts\)) {
+    New-Item -ItemType Directory -Force -Path $env:programdata\TRMMScripts\
 }
 
 If (!(test-path 'C:\Program Files\TacticalAgent\bluescreenview.exe')) {
-cd c:\temp
-Invoke-WebRequest https://www.nirsoft.net/utils/bluescreenview.zip -Outfile bluescreenview.zip
-expand-archive bluescreenview.zip
-cd C:\TEMP\bluescreenview\
-move .\bluescreenview.exe 'C:\Program Files\TacticalAgent\'
+    cd c:\temp
+    Invoke-WebRequest https://www.nirsoft.net/utils/bluescreenview.zip -Outfile bluescreenview.zip
+    expand-archive bluescreenview.zip
+    cd C:\TEMP\bluescreenview\
+    move .\bluescreenview.exe 'C:\Program Files\TacticalAgent\'
 
-start sleep -Seconds 5
+    start sleep -Seconds 5
 
-Remove-Item -LiteralPath "c:\temp\bluescreenview.zip" -Force -Recurse
-& 'C:\Program Files\TacticalAgent\bluescreenview.exe' /stext "$env:programdata\TRMMScripts\crashes.txt"
-get-content '$env:programdata\TRMMScripts\crashes.txt'
+    Remove-Item -LiteralPath "c:\temp\bluescreenview.zip" -Force -Recurse
+    & 'C:\Program Files\TacticalAgent\bluescreenview.exe' /stext "$env:programdata\TRMMScripts\crashes.txt"
+    get-content '$env:programdata\TRMMScripts\crashes.txt'
 }
 
 else {
-& 'C:\Program Files\TacticalAgent\bluescreenview.exe' /stext "$env:programdata\TRMMScripts\crashes.txt"
-get-content '$env:programdata\TRMMScripts\crashes.txt'
+    & 'C:\Program Files\TacticalAgent\bluescreenview.exe' /stext "$env:programdata\TRMMScripts\crashes.txt"
+    get-content '$env:programdata\TRMMScripts\crashes.txt'
 }
 
 
-#################
+################# Alternate method
+
+
 try {
     Invoke-WebRequest -Uri "https://www.nirsoft.net/utils/bluescreenview.zip" -OutFile "$($ENV:Temp)\bluescreeview.zip"
     Expand-Archive "$($ENV:Temp)\bluescreeview.zip" -DestinationPath "$($ENV:Temp)" -Force
@@ -41,7 +42,7 @@ catch {
 }
  
 $BSODs = get-content "$($ENV:Temp)\Export.csv" | ConvertFrom-Csv -Delimiter ',' `
- -Header Dumpfile, Timestamp, Reason, Errorcode, Parameter1, Parameter2, Parameter3, Parameter4, CausedByDriver | foreach-object { $_.Timestamp = [datetime]::Parse($_.timestamp, [System.Globalization.CultureInfo]::CurrentCulture); $_ }
+    -Header Dumpfile, Timestamp, Reason, Errorcode, Parameter1, Parameter2, Parameter3, Parameter4, CausedByDriver | foreach-object { $_.Timestamp = [datetime]::Parse($_.timestamp, [System.Globalization.CultureInfo]::CurrentCulture); $_ }
 Remove-item "$($ENV:Temp)\Export.csv" -Force
  
 #$BSODFilter = $BSODs | where-object { $_.Timestamp -gt ((get-date).addhours(-24)) }
@@ -49,7 +50,7 @@ $BSODFilter = $BSODs
  
 if (!$BSODFilter) {
     #write-host "Healthy - No BSODs found in the last 24 hours"
-	write-host "Healthy - No BSODs found"
+    write-host "Healthy - No BSODs found"
 }
 else {
     write-host "Unhealthy - BSOD found. Check Diagnostics"
