7e962856669c4e14f838b2ddc477587108befa80
dinger1986 <daniel.lamb@flonix.co.uk>
2022-04-03 14:37:49 +0100

    Update Win_Laptop_Battery_Create_Detailed_Report.bat

--- scripts_wip/Win_Laptop_Battery_Create_Detailed_Report.bat
+++ scripts_wip/Win_Laptop_Battery_Create_Detailed_Report.bat
@@ -1,1 +1,8 @@
-powercfg /batteryreport /output "C:\battery-report.html"
+If(!(test-path $env:programdata\RMMScripts\))
+{
+      New-Item -ItemType Directory -Force -Path $env:programdata\TRMMScripts\
+}
+
+powercfg /batteryreport /output "$env:programdata\TRMMScripts\battery-report.txt"
+
+get-content '$env:programdata\TRMMScripts\battery-report.txt'
