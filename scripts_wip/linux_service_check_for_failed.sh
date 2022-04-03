d9728f0adae1e4e3a8c389449ee57884be135f23
dinger1986 <daniel.lamb@flonix.co.uk>
2022-04-03 22:14:17 +0100

    Update linux_service_check_for_failed.sh

--- scripts_wip/linux_service_check_for_failed.sh
+++ scripts_wip/linux_service_check_for_failed.sh
@@ -2,4 +2,11 @@
 
 # Returns 1 if there are failed services, and listed failed services
 
+HAS_SYSTEMD=$(ps --no-headers -o comm 1)
+if [ "${HAS_SYSTEMD}" != 'systemd' ]; then
+    echo "This install script only supports systemd"
+    echo "Please install systemd or manually create the service using your systems's service manager"
+    exit 0
+fi
+
 systemctl  --failed
