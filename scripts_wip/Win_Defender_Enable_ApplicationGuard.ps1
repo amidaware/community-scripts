f5cd6f61b4489e5282c5cc113b635d23837187a5
dinger1986 <daniel.lamb@flonix.co.uk>
2022-04-03 21:14:32 +0100

    Update Win_Defender_Enable_ApplicationGuard.ps1

--- scripts_wip/Win_Defender_Enable_ApplicationGuard.ps1
+++ scripts_wip/Win_Defender_Enable_ApplicationGuard.ps1
@@ -3,9 +3,30 @@
 # Restart Required to complete install.
 #
 # Sets Variable for feature to be installed.
+
+<#
+      .SYNOPSIS
+      Script to Install Windows Defender Application Guard Feature. 
+      .PARAMETER Mode
+      The Enable is assumed, to disable feature use -mode disable
+      .EXAMPLE
+      -FeatureName NameofFeature -mode disable
+      .EXAMPLE
+      -FeatureName NameofFeature
+  #>
+
+param (
+    [string] $Mode
+)
 $FeatureName = "Windows-Defender-ApplicationGuard"  
 
-# If Feature Installed already then skips otherwise installs.
+if ($Mode -eq "disable") {
+    Write-Output "Disabling $FeatureName"
+    Disable-WindowsOptionalFeature -online -FeatureName $FeatureName -NoRestart
+}
+
+else {
+    # If Feature Installed already then skips otherwise installs.
 if((Get-WindowsOptionalFeature -FeatureName $FeatureName -Online).State -eq "Enabled") {
 
         write-output "Windows Defender Application Guard Installed"
@@ -17,4 +38,4 @@ if((Get-WindowsOptionalFeature -FeatureName $FeatureName -Online).State -eq "Ena
 Enable-WindowsOptionalFeature -online -FeatureName $FeatureName -NoRestart
 
     }
-    
+}
