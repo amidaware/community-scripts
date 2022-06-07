acc20ecbfe8c0e10cc8e8ca6f3c5bc776d3606f9
silversword411 <github.com@datmail.com>
2022-06-07 17:06:25 -0400

    add python ping check script

---
+++ scripts_wip/ping_check.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+if len(sys.argv) != 2:
+    print("ERROR: Missing hostname or ip argument")
+    sys.exit(1)
+
+cmd = ["ping.exe", sys.argv[1], "-n", "5"]
+
+r = subprocess.run(cmd, capture_output=True)
+
+success = ["Reply", "bytes", "time", "TTL"]
+
+print(r.stdout.decode())
+
+if all (i in r.stdout.decode() for i in success):
+    sys.exit(0)
+
+sys.exit(1)
