6904537aed1e6e434f30256fe1e954c57faff2c3
jcpit <jake@jcpit.com.au>
2022-06-16 09:51:31 +1000

    Update Win_Bluescreen_Report.ps1
    Fixed up IF statement.

--- scripts/Win_Bluescreen_Report.ps1
+++ scripts/Win_Bluescreen_Report.ps1
@@ -26,10 +26,7 @@ if (Get-WinEvent -FilterHashtable @{LogName = 'application'; ID = '1001'; Provid
     Write-Output "There has been bluescreen events detected on your system"
     Get-WinEvent -FilterHashtable @{LogName = 'application'; ID = '1001'; ProviderName = 'Windows Error Reporting'; Level = 4; Data = 'BlueScreen'; StartTime = $TimeSpan }
     exit 1
-}
-
-{
-    else 
+} else {
     Write-Output "No bluescreen events detected in the past 24 hours."
     exit 0
 }
