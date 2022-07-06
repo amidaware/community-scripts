fbba25d944a70addfbc3764f3167bb5aa7a63cf9
silversword411 <github.com@datmail.com>
2022-07-06 14:12:15 -0400

    Adding TRMM Agent Debug mode to official library

--- scripts/Win_TRMM_Agent_Debugmode.bat
+++ scripts/Win_TRMM_Agent_Debugmode.bat
@@ -2,10 +2,11 @@ rem This will stop the TRMM services and manually launch TRMM in debug mode.
 rem You can then use Win_TRMM_GetLogs.ps1 to collect as needed
 rem Restart the computer to stop debug and return agent to regular mode
 
+del "C:\Program Files\TacticalAgent\runasdebug.bat"
 (
 echo REM Stop TRMM services and start with debugging
 echo net stop "tacticalrmm"
-echo start "" "C:\Program Files\TacticalAgent\tacticalrmm.exe" -m svc -log debug
+echo start "" "C:\Program Files\TacticalAgent\tacticalrmm.exe" -m rpc -log debug
 )>"C:\Program Files\TacticalAgent\runasdebug.bat"
 
 start "" "C:\Program Files\TacticalAgent\runasdebug.bat"
