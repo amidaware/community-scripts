c4a978d92bda7db67ed6f00ad8dba1dd9514555c
silversword411 <github.com@datmail.com>
2022-07-13 00:35:21 -0400

    adding documentation

--- scripts_wip/Win_User_Admin_LAPS.ps1
+++ scripts_wip/Win_User_Admin_LAPS.ps1
@@ -1,26 +1,44 @@
 <#
 
 .SYNOPSIS
-   For Setting up and running a LAPS (Local Administrator Password Solution)
+   For Setting up and running a LAPS (Local Administrator Password Solution) for non-AD
+   https://www.microsoft.com/en-us/download/details.aspx?id=46899
 
 .DESCRIPTION
-   Long description
-   eg Checks IP address on all local network adapters, and returns results
-
-.EXAMPLE
-   Example of how to use this cmdlet
+   This will create a local administrator, with each run it will change that localadmin user's password. This will let you make users non-administrators, but still have a local admin account.  
+   Walkthru here: https://docs.tacticalrmm.com/functions/examples/#setup-laps-local-administrator-password-solution
 
 .PARAMETER LAPSID
-   If you want a custom Local Username, create a TRMM Global Key called: LAPS_username otherwise LocalAdmin will be used: 
+   If you want a custom Local Username used server wide, create a TRMM Global Key called: LAPS_username otherwise LocalAdmin will be used:
    -LAPSID {{global.LAPS_username}}
+   -LAPSID customadminname
 
 .PARAMETER PassLength
-   If you want a password with a different length use: 
+   If you want a password with a length other than 16 use:
    -PassLength 20
 
 .PARAMETER HideUser
-    If you want to hide the login username from login screen use:
+    If you want to hide the login username from the windows login screen use:
     -HideUser
+
+.PARAMETER ShowUser
+    Show the username on the login screen:
+    -ShowUser
+
+.EXAMPLE
+   -LAPSID {{global.LAPS_username}}
+
+.EXAMPLE
+   -LAPSID corpadmin
+
+.EXAMPLE
+   -LAPSID {{global.LAPS_username}} -HideUser
+
+.EXAMPLE
+   -LAPSID {{global.LAPS_username}} -ShowUser
+
+.EXAMPLE
+   -LAPSID {{global.LAPS_username}} -PassLength 20 -HideUser
 
 .OUTPUTS
    The current LAPS admin password. Use as a collector script to write it to a custom_field
@@ -30,23 +48,20 @@
    v2.0 silversword adding TRMM parameters, documenting etc
 #>
 
-# LAPS script from @subz
-# TODO: Needs comments, and parameters for New Admin. Merge/consolidate with other admin Local Administrator Password Scripts if possible
-# Add hiding login ID https://www.windowscentral.com/how-hide-specific-user-accounts-sign-screen-windows-10
 
 param(
     [string]$LAPSID,
     [Int]$PassLength,
-    [Switch]$HideUser = $false
+    [Switch]$HideUser = $false,
+    [Switch]$ShowUser = $false
 )
 
-# Hide errors when noone is logged in
+
 #$ErrorActionPreference= 'silentlycontinue'
 
 $ComputerName = (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object name).name
 
 #####################################################################
-
 
 if (!($LAPSID)) {
     Write-Output "No LAPSID defined. Using LocalAdmin"
@@ -62,31 +77,37 @@ if (!($PassLength)) {
 
 Write-Output "LAPSID is: $LAPSID"
 
+if ($ShowUser) {
+    Write-Output "Showing User on Login page and exit"
+    New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name $LAPSID -Value 1 -Force
+    Exit 0
+}
+
+
 add-type -AssemblyName System.Web
 $LocalAdminPassword = [System.Web.Security.Membership]::GeneratePassword($PassLength, 2)
 
-$checkuser = Get-LocalUser | where-Object Name -eq $LAPSID | Measure
+$checkuser = Get-LocalUser | where-Object Name -eq $LAPSID | Measure-Object
 if ($checkuser.Count -eq 0) {
     Write-Output "User $($LAPSID) was not found. Creating User and adding to Admin group"
     New-LocalUser -Name $LAPSID -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
     Add-LocalGroupMember -Group Administrators -Member $LAPSID
-
-    if ($HideUser) {
-        Write-Output "HideUser enabled. Writing reg key" 
-        if ((Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList") -ne $true) {  
-            New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -force -ea SilentlyContinue 
-        };
-        New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name $LAPSID -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
-    }
-    else {
-        Write-Output "No HideUser, Skipping"
-    }
 }
 else {
     Write-Output "User $($LAPSID) was found. Setting password for existing user."
     Set-LocalUser -Name $LAPSID -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force)
 }
 
+if ($HideUser) {
+    Write-Output "HideUser enabled. Writing reg key" 
+    if ((Test-Path -LiteralPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList") -ne $true) {  
+        New-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -force -ea SilentlyContinue 
+    };
+    New-ItemProperty -LiteralPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name $LAPSID -Value 0 -PropertyType DWord -Force -ea SilentlyContinue;
+}
+else {
+    Write-Output "No HideUser, Skipping"
+}
 
 
 #Write-Output "The $LAPSID account has been enabled on $($ComputerName) and a new password has been set"
