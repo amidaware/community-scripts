f0489d3c2777344fa58f77b64ab4f3d42df257cb
wh1te909 <dcparsi@gmail.com>
2022-10-18 04:29:11 +0000

    add stop/start options to screenconnect script

--- scripts/Win_ScreenConnectAIO.ps1
+++ scripts/Win_ScreenConnectAIO.ps1
@@ -1,9 +1,10 @@
 <#
-Requires global variables for serviceName "ScreenConnectService" and url "ScreenConnectInstaller"'
+Requires global variables for serviceName "ScreenConnectService" and url "ScreenConnectInstaller"
 serviceName is the name of the ScreenConnect Service once it is installed EG: "ScreenConnect Client (1327465grctq84yrtocq)"
-url is the path the download the exe version of the ScreenConnect Access installer'
-Both variables values must start and end with " (Prior to TRMM Version 0.6.5), remove / don't use " on TRMM Version 0.6.5 or later.
-Also accepts uninstall variable to remove the installed instance if required. 
+url is the path the download the exe version of the ScreenConnect Access installer
+Both variables values must start and end with "
+Also accepts uninstall variable to remove the installed instance if required.
+2022-10-12: Added -action start and -action stop variables
 #>
 
 param (
@@ -36,6 +37,46 @@ exit 1
 if ($action -eq "uninstall") {
         $MyApp = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "$serviceName"}
         $MyApp.Uninstall()
+} elseif ($action -eq "stop") {
+    If ((Get-Service $serviceName).Status -eq 'Running') {
+            Try
+            {
+            Write-Output "Stopping $serviceName"
+            Set-Service -Name $serviceName -Status stopped -StartupType disabled
+            exit 0
+            }
+            Catch
+                {
+                $ErrorMessage = $_.Exception.Message
+                $FailedItem = $_.Exception.ItemName
+                Write-Error -Message "$ErrorMessage $FailedItem"
+                exit 1
+                }
+            Finally
+                {
+                }
+
+        }
+} elseif ($action -eq "start") {
+If ((Get-Service $serviceName).Status -ne 'Running') {  
+    Try
+            {
+            Write-Host "Starting $serviceName"
+            Set-Service -Name $serviceName -Status running -StartupType automatic
+            exit 0
+            }
+            Catch
+                {
+                $ErrorMessage = $_.Exception.Message
+                $FailedItem = $_.Exception.ItemName
+                Write-Error -Message "$ErrorMessage $FailedItem"
+                exit 1
+                }
+            Finally
+                {
+                }
+
+        }
 } else {
     If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
 
@@ -87,9 +128,13 @@ if ($action -eq "uninstall") {
         {
 			    $start_time = Get-Date
 			    $wc = New-Object System.Net.WebClient
-			    $wc.DownloadFile("$url&c=$company&c=$site", "$OutPath\$output")
-            Start-Process -FilePath $OutPath\$output -Wait
-			    Write-Output "Time taken to download and install: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+			    $wc.DownloadFile("$url", "$OutPath\$output")
+			    Write-Output "Time taken to download: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+			    
+			    $start_time = Get-Date
+			    $wc = New-Object System.Net.WebClient 
+			    Start-Process -FilePath $OutPath\$output -Wait
+			    Write-Output "Time taken to install: $((Get-Date).Subtract($start_time).Seconds) second(s)"           
             exit 0
         }
         Catch
