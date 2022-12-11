df130b6a05e7ddaeb7c310304a4da12296e93683
silversword411 <github.com@datmail.com>
2022-12-11 16:46:01 -0500

    wip updating

--- scripts_wip/Win_Boot_UEFI_or_LegacyBIOS.ps1
+++ scripts_wip/Win_Boot_UEFI_or_LegacyBIOS.ps1
@@ -1,19 +1,17 @@
 # Check if the system is using UEFI or legacy BIOS
-if ([System.Management.ManagementBaseObject]::ReferenceEquals(
-    (Get-WmiObject -Class "Win32_BIOS" -Namespace "root\CIMV2").BIOSVersion,
-    (Get-WmiObject -Class "Win32_BIOS" -Namespace "root\CIMV2").SMBIOSBIOSVersion
-    )) {
-    Write-Output "The system is using UEFI"
-
-    # Check if Secure Boot is enabled
+if ($env:firmware_type -match "UEFI") {
+    # If the system is using UEFI, check if secure boot is enabled
     $secureBootSetting = (Get-WmiObject -Class "Win32_BIOS" -Namespace "root\CIMV2").SecureBootEnabled
     if ($secureBootSetting -eq "True") {
-        Write-Output "Secure Boot is enabled"
+        # If secure boot is enabled, output a message
+        Write-Output "The system is using UEFI with secure boot enabled."
     }
     else {
-        Write-Output "Secure Boot is not enabled"
+        # If secure boot is not enabled, output a message
+        Write-Output "The system is using UEFI but secure boot is not enabled."
     }
 }
 else {
-    Write-Output "The system is using legacy BIOS"
+    # If the system is not using UEFI, output a message
+    Write-Output "The system is using legacy BIOS."
 }
