2555ec53453141885c9d26a2ae4b08e9a085f880
Joel DeTeves <joel@lcsnetworks.com>
2023-04-17 11:10:41 -0700

    Add online disk check / repair script (powershell)

---
+++ scripts/Win_Disk_Check_Repair.ps1
@@ -0,0 +1,56 @@
+<#
+.SYNOPSIS
+    A PowerShell script to repair a volume using the Repair-Volume cmdlet.
+
+.DESCRIPTION
+    This script uses the Repair-Volume cmdlet to scan and/or repair a specified volume
+    based on the provided DriveLetter. By default, the script performs an online scan.
+    The user can choose to perform an offline scan or a spot fix on the volume.
+
+.PARAMETER DriveLetter
+    Specifies the drive letter of the volume to be scanned/repaired.
+
+.PARAMETER Offline
+    When specified, the script performs an offline scan on the volume.
+
+.PARAMETER SpotFix
+    When specified, the script performs a spot fix on the volume.
+
+.EXAMPLE
+    .\RepairVolume.ps1 -DriveLetter "C"
+
+    Performs an online scan of the volume with the drive letter "C"
+
+.EXAMPLE
+    .\RepairVolume.ps1 -DriveLetter "C" -Offline
+
+    Schedules an offline scan of the volume with the drive letter "C" upon next reboot
+
+.EXAMPLE
+    .\RepairVolume.ps1 -DriveLetter "C" -SpotFix
+
+    Performs a spot fix on the volume with the drive letter "C"
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$DriveLetter,
+
+    [switch]$Offline,
+    [switch]$SpotFix
+)
+
+# Perform the requested operation(s) on the volume
+if ($Offline) {
+    Write-Host "Performing offline scan on volume $DriveLetter`:"
+    Repair-Volume -DriveLetter $DriveLetter -OfflineScanAndFix
+}
+elseif ($SpotFix) {
+    Write-Host "Performing spot fix on volume $DriveLetter`:"
+    Repair-Volume -DriveLetter $DriveLetter -SpotFix
+}
+else {
+    Write-Host "Performing online scan on volume $DriveLetter`:"
+    Repair-Volume -DriveLetter $DriveLetter -Scan
+}
