859a55634c94f916120b2dd2152ee9d8aac9b6bc
silversword411 <github.com@datmail.com>
2023-06-02 09:55:24 -0400

    adding comment headers

--- scripts/Win_Azure_Mars_Cloud_Backup_Status.ps1
+++ scripts/Win_Azure_Mars_Cloud_Backup_Status.ps1
@@ -1,3 +1,14 @@
+<#
+    .SYNOPSIS
+    Check for errors in Cloud Backup Mars and returns results.
+
+    .DESCRIPTION
+    This script checks for errors in the CloudBackup/Operational log on all local network adapters for the past 24 hours, and returns the results. If errors are found, the script outputs "Cloud Backup Mars Ended with Errors" and displays the relevant log events. If no errors are found, the script outputs "Cloud Backup Mars Backup Is Working Correctly" and displays the relevant log events.
+
+    .NOTES
+    Version: 1.0 
+#>
+
 $ErrorActionPreference = 'silentlycontinue'
 $TimeSpan = (Get-Date) - (New-TimeSpan -Day 1)
 
