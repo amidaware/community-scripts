9cbca4dedfc1595cc970ec530ad0d6ebe227d4dd
silversword411 <github.com@datmail.com>
2023-06-06 02:59:08 -0400

    adding comments and parameter switch for single vs multiple

--- scripts_staging/Win_Take_Screenshot.ps1
+++ scripts_staging/Win_Take_Screenshot.ps1
@@ -1,4 +1,20 @@
-## Take screenshot of curently logged in user, will not work on RDS with multiple sessions
+<#
+.SYNOPSIS
+    Take screenshot of currently logged in user.
+
+.DESCRIPTION
+    This script captures a screenshot of the currently logged in user's screen. Will not work on RDS with multiple sessions.
+
+.PARAMETER single
+    Specifies whether to remove old screenshots before taking a new one.
+
+.EXAMPLE
+    -single
+    Captures a screenshot of the currently logged in user's screen and removes any existing screenshots.
+
+.NOTES
+    Version: 1.0 The screenshots are saved in the TacticalRMM scripts/screenshots directory.
+#>
 
 if (Get-PackageProvider -Name NuGet) {
     Write-Output "NuGet Already Installed"
@@ -16,8 +32,8 @@ else {
     Install-Module -Name RunAsUser -Force
 }
 
-If (!(test-path "$env:programdata\TRMMScripts\screenshots\")) {
-    New-Item -ItemType Directory -Force -Path "$env:programdata\TRMMScripts\screenshots\"
+If (!(test-path "$env:programdata\TacticalRMM\scripts\screenshots\")) {
+    New-Item -ItemType Directory -Force -Path "$env:programdata\TacticalRMM\scripts\screenshots\"
 }
 
 If (!(test-path "$env:programdata\Tactical RMM\temp\curpsxpolicy.txt")) {
@@ -25,39 +41,39 @@ If (!(test-path "$env:programdata\Tactical RMM\temp\curpsxpolicy.txt")) {
 
     (
         Write-Output $curexpolicy
-    )>"$env:programdata\TRMMScripts\curpsxpolicy.txt"
+    )>"$env:programdata\TacticalRMM\scripts\curpsxpolicy.txt"
 }
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell -Name ExecutionPolicy -Value Unrestricted
 
 #Remove old screenshots before taking a new one
-Remove-Item "$env:programdata\TRMMScripts\screenshots\*.png"
+Remove-Item "$env:programdata\TacticalRMM\scripts\screenshots\*.png"
 
 Invoke-AsCurrentUser -scriptblock {
-$File = 'C:\temp\Screenshot1.bmp'
+    $File = 'C:\TacticalRMM\temp\Screenshot1.bmp'
 
-Add-Type -AssemblyName System.Windows.Forms
-Add-type -AssemblyName System.Drawing
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
 
-# Gather Screen resolution information
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    # Gather Screen resolution information
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
 
-# Create bitmap using the top-left and bottom-right bounds
-$bitmap = New-Object System.Drawing.Bitmap $Screen.Width, $Screen.Height
+    # Create bitmap using the top-left and bottom-right bounds
+    $bitmap = New-Object System.Drawing.Bitmap $Screen.Width, $Screen.Height
 
-# Create Graphics object
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    # Create Graphics object
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
 
-# Capture screen
-$graphic.CopyFromScreen($Screen.Left, $Screen.Top, 0, 0, $bitmap.Size)
+    # Capture screen
+    $graphic.CopyFromScreen($Screen.Left, $Screen.Top, 0, 0, $bitmap.Size)
 
-# Save to file
-$screen_file = "$env:programdata\TRMMScripts\screenshots\" + $env:computername + "_" + $env:username + "_" + "$((get-date).tostring('yyyy.MM.dd-HH.mm.ss')).png"
-$bitmap.Save($screen_file, [System.Drawing.Imaging.ImageFormat]::Png)
+    # Save to file
+    $screen_file = "$env:programdata\TacticalRMM\scripts\screenshots\" + $env:computername + "_" + $env:username + "_" + "$((get-date).tostring('yyyy.MM.dd-HH.mm.ss')).png"
+    $bitmap.Save($screen_file, [System.Drawing.Imaging.ImageFormat]::Png)
 }
 
 Write-Output "Successfully saved screenshot"
 
-$curpsxpol = Get-Content -Path "$env:programdata\TRMMScripts\curpsxpolicy.txt";
+$curpsxpol = Get-Content -Path "$env:programdata\TacticalRMM\scripts\curpsxpolicy.txt";
     
 Set-ExecutionPolicy -ExecutionPolicy $curpsxpol
 
