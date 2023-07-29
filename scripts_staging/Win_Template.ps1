f287dd513def1bf65a7c5d7f63d31d0e389e7aa5
David Randall <David@NiceGuyIT.biz>
2023-07-29 17:03:26 -0400

    Add: Test-PowerShellVersion, Test-IsAdmin, Test-IsInteractiveShell

--- scripts_staging/Win_Template.ps1
+++ scripts_staging/Win_Template.ps1
@@ -51,7 +51,7 @@ function Set-LogLevel() {
         [string] $LogLevel
     )
 
-    switch ( $LogLevel.ToLower()) {
+    switch -wildcard ( $LogLevel.ToLower()) {
         'error' {
             Set-Variable -Scope Global -Name ErrorActionPreference -Value 'Continue'
             Set-Variable -Scope Global -Name WarningPreference -Value 'SilentlyContinue'
@@ -66,7 +66,8 @@ function Set-LogLevel() {
             Set-Variable -Scope Global -Name VerbosePreference -Value 'SilentlyContinue'
             Set-Variable -Scope Global -Name DebugPreference -Value 'SilentlyContinue'
         }
-        'info' {
+        # The preference variable is "information". Accept "information" and other variants of "info"
+        'info*' {
             Set-Variable -Scope Global -Name ErrorActionPreference -Value 'Continue'
             Set-Variable -Scope Global -Name WarningPreference -Value 'Continue'
             Set-Variable -Scope Global -Name InformationPreference -Value 'Continue'
@@ -95,7 +96,7 @@ function Set-LogLevel() {
             Set-Variable -Scope Global -Name VerbosePreference -Value 'SilentlyContinue'
             Set-Variable -Scope Global -Name DebugPreference -Value 'SilentlyContinue'
             # Log the warning after changing the log level. Otherwise the output is ignored (SilentlyContinue).
-            Write-Warning ('Undefined LogLevel {0}; Using default of Info' -f $LogLevel)
+            Write-Warning ('Set-LogLevel(): Undefined LogLevel {0}; Using default of Info' -f $LogLevel)
         }
     }
 }
@@ -113,18 +114,18 @@ function Get-LogLevel() {
     param ()
 
     Write-Output ''
-    Write-Output 'Here is the list of logging preferences:'
-    Write-Output ('ErrorActionPreference: {0}' -f $PSCmdlet.GetVariableValue('ErrorActionPreference'))
-    Write-Output ('WarningPreference: {0}' -f $PSCmdlet.GetVariableValue('WarningPreference'))
-    Write-Output ('InformationPreference: {0}' -f $PSCmdlet.GetVariableValue('InformationPreference'))
-    Write-Output ('VerbosePreference: {0}' -f $PSCmdlet.GetVariableValue('VerbosePreference'))
-    Write-Output ('DebugPreference: {0}' -f $PSCmdlet.GetVariableValue('DebugPreference'))
+    Write-Output 'Get-LogLevel(): Here is the list of logging preferences:'
+    Write-Output ('Get-LogLevel(): ErrorActionPreference: {0}' -f $PSCmdlet.GetVariableValue('ErrorActionPreference'))
+    Write-Output ('Get-LogLevel(): WarningPreference: {0}' -f $PSCmdlet.GetVariableValue('WarningPreference'))
+    Write-Output ('Get-LogLevel(): InformationPreference: {0}' -f $PSCmdlet.GetVariableValue('InformationPreference'))
+    Write-Output ('Get-LogLevel(): VerbosePreference: {0}' -f $PSCmdlet.GetVariableValue('VerbosePreference'))
+    Write-Output ('Get-LogLevel(): DebugPreference: {0}' -f $PSCmdlet.GetVariableValue('DebugPreference'))
 
     Write-Output ''
-    Write-Output 'Here is a list of some other preferences:'
-    Write-Output ('ConfirmPreference: {0}' -f $PSCmdlet.GetVariableValue('ConfirmPreference'))
-    Write-Output ('ProgressPreference: {0}' -f $PSCmdlet.GetVariableValue('ProgressPreference'))
-    Write-Output ('ErrorView: {0}' -f $PSCmdlet.GetVariableValue('ErrorView'))
+    Write-Output 'Get-LogLevel(): Here is a list of some other preferences:'
+    Write-Output ('Get-LogLevel(): ConfirmPreference: {0}' -f $PSCmdlet.GetVariableValue('ConfirmPreference'))
+    Write-Output ('Get-LogLevel(): ProgressPreference: {0}' -f $PSCmdlet.GetVariableValue('ProgressPreference'))
+    Write-Output ('Get-LogLevel(): ErrorView: {0}' -f $PSCmdlet.GetVariableValue('ErrorView'))
 }
 
 # Set the log level of the script. "-LogLevel Debug" will output all Write statements.
@@ -148,8 +149,9 @@ if ($LogLevel) {
     Write-Verbose '--------------------------------------------------'
 }
 
+
 <# ================================================================================ #>
-function Test-TlsVersion($MinTlsVersion) {
+function Test-TlsVersion([string]$MinTlsVersion = "Tls12") {
     # Test if TLS 1.2 is supported
     try {
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::$MinTlsVersion
@@ -161,8 +163,72 @@ function Test-TlsVersion($MinTlsVersion) {
         return $False
     }
 }
-Test-TlsVersion
+Test-TlsVersion "Tls"
 
+
+<# ================================================================================ #>
+function Test-PowerShellVersion([string]$MinPsVersion = '2.0') {
+    <#
+    This function tests for the PowerShell version. This is the $PsVersionTable from Windows 7.
+    Name                           Value
+    ----                           -----
+    CLRVersion                     2.0.50727.8806
+    BuildVersion                   6.1.7601.17514
+    PSVersion                      2.0
+    WSManStackVersion              2.0
+    PSCompatibleVersions           {1.0, 2.0}
+    SerializationVersion           1.1.0.1
+    PSRemotingProtocolVersion      2.1
+    #>
+    if ($PsVersionTable.PSVersion -ge $MinPsVersion) {
+        Write-Output ('Test-PowerShellVersion(): PowerShell version "{0}" supported' -f $PsVersionTable.PSVersion)
+        Write-Verbose ('Test-PowerShellVersion(): Requested minimum version "{0}"' -f $MinPsVersion)
+        return $True
+    }
+    else {
+        Write-Warning ('Test-PowerShellVersion(): PowerShell version "{0}" is not supported' -f $PsVersionTable.PSVersion)
+        Write-Verbose ('Test-PowerShellVersion(): Requested minimum version "{0}"' -f $MinPsVersion)
+        return $False
+    }
+}
+Test-PowerShellVersion '8.0'
+
+
+<# ================================================================================ #>
+function Test-IsAdmin() {
+    # Test-Admin will return true if the script is running as an administrator.
+    # Mandatory Label\High Mandatory Level is when the script is run from an elevated session.
+    # Mandatory Label\System Mandatory Level is when the script is run from SYSTEM.
+    $Whoami = & "C:/Windows/System32/whoami.exe" /groups
+    if (($Whoami -match "Mandatory Label\\High Mandatory Level") -or
+            ($Whoami -match "Mandatory Label\\System Mandatory Level")) {
+        return $true
+    }
+    return $false
+}
+if (Test-IsAdmin) {
+    Write-Verbose ('Test-IsAdmin(): Is an administrator')
+}
+
+
+<# ================================================================================ #>
+function Test-IsInteractiveShell {
+    # https://stackoverflow.com/questions/9738535/powershell-test-for-noninteractive-mode
+    # Test each Arg for match of abbreviated '-NonInteractive' command.
+    $NonInteractive = [Environment]::GetCommandLineArgs() | Where-Object{ $_ -like '-NonI*' }
+
+    if ([Environment]::UserInteractive -and -not$NonInteractive) {
+        # We are in an interactive shell.
+        return $true
+    }
+    return $false
+}
+if (Test-IsInteractiveShell) {
+    Write-Verbose ('Test-IsInteractiveShell(): Is an interactive shell')
+}
+
+
+<# ================================================================================ #>
 Function InstallRunAsUserRequirements {
     # Install Requirements for RunAsUser
     if (!(Get-PackageProvider -Name NuGet -ListAvailable)) {
@@ -180,11 +246,11 @@ Function InstallRunAsUserRequirements {
         Write-Output "RunAsUser already installed"
     }
 }
-InstallRunAsUserRequirements
+#InstallRunAsUserRequirements
 
-Invoke-AsCurrentUser -scriptblock {
+#Invoke-AsCurrentUser -scriptblock {
     # RunAsUser
-}
+#}
 
 
 function Set-RegistryValue ($registryPath, $name, $value) {
@@ -198,4 +264,4 @@ function Set-RegistryValue ($registryPath, $name, $value) {
 }
 # $RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Edge"
 # Set-RegistryValue -registryPath $RegistryPath -name "PersonalizationReportingEnabled" -value 0
-Set-RegistryValue
+#Set-RegistryValue
