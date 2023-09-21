46d35f261d5123457451caeaf6c96244fb076945
SpookOz <50608124+SpookOz@users.noreply.github.com>
2023-09-21 10:58:56 +1000

    Update Mac_Users_List.sh
    Added code to filter out system users so the result only shows actual human users.

--- scripts_wip/Mac_Users_List.sh
+++ scripts_wip/Mac_Users_List.sh
@@ -1,1 +1,7 @@
-/usr/bin/dscl . -list /Users
+#!/usr/bin/env bash
+
+# Old code
+# /usr/bin/dscl . -list /Users
+
+# New code to list Mac users and filters out the system users
+/usr/bin/dscl . -list /Users | grep -v '^_'
