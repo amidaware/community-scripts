da9d1e39fe10a9c110b1fbb154c9c612d2f4703b
redanthrax <redanthraxx@gmail.com>
2024-04-16 08:38:19 -0700

    added logic for upgrade, updated params

--- scripts_wip/Win_PerchLogShipper_Install.ps1
+++ scripts_wip/Win_PerchLogShipper_Install.ps1
@@ -13,34 +13,90 @@
     3. Create the follow script arguments
         a) -Token {{client.PerchToken}}
 .NOTES
-   Version: 1.0
+   Version: 1.1
    Author: redanthrax
    Creation Date: 2022-04-08
+   Update Date: 2024-04-16
 #>
 
 Param(
-    [Parameter(Mandatory)]
     [string]$Token,
+
+    [string]$LatestVersion = "2023.05.12",
 
     [switch]$Uninstall
 )
 
+function Compare-SoftwareVersion {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Version1,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Version2
+    )
+
+    # Split the version strings into individual parts
+    $versionParts1 = $Version1 -split '\.'
+    $versionParts2 = $Version2 -split '\.'
+
+    # Get the minimum number of parts between the two versions
+    $minParts = [Math]::Min($versionParts1.Count, $versionParts2.Count)
+
+    # Compare the version parts
+    for ($i = 0; $i -lt $minParts; $i++) {
+        $part1 = [int]$versionParts1[$i]
+        $part2 = [int]$versionParts2[$i]
+
+        if ($part1 -gt $part2) {
+            return $true
+        }
+        elseif ($part1 -lt $part2) {
+            return $false
+        }
+    }
+
+    # If all parts are equal, check the length of the version strings
+    if ($versionParts1.Count -gt $versionParts2.Count) {
+        # Check the additional part in Version1
+        $additionalPart = $versionParts1[$minParts..($versionParts1.Count - 1)] -join '.'
+        return ![string]::IsNullOrEmpty($additionalPart)
+    }
+    elseif ($versionParts1.Count -lt $versionParts2.Count) {
+        # Check the additional part in Version2
+        $additionalPart = $versionParts2[$minParts..($versionParts2.Count - 1)] -join '.'
+        return [string]::IsNullOrEmpty($additionalPart)
+    }
+
+    return $true
+}
+
 function Win_PerchLogShipper_Install {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "InstallSet")]
     Param(
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory = $true, ParameterSetName = "InstallSet")]
         [string]$Token,
 
+        [string]$LatestVersion,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "UninstallSet")]
         [switch]$Uninstall
     )
 
     Begin {
+        $Upgrade = $false
         $Apps = @()
         $Apps += Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
         $Apps += Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
         if ($null -ne (Get-Service | Where-Object { $_.DisplayName -Match "perch" }) -and -Not($Uninstall)) {
-            Write-Output "Perch already installed."
-            Exit 0
+            $perch = $Apps | Where-Object { $_.DisplayName -Match "perch"}
+            if (Compare-SoftwareVersion $perch.DisplayVersion $LatestVersion) {
+                Write-Output "Perch $($perch.DisplayVersion) already installed."
+                Exit 0
+            }
+            else {
+                $Upgrade = $true
+            }
         }
 
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -64,6 +120,13 @@ function Win_PerchLogShipper_Install {
                 }
             }
 
+            if ($Upgrade) {
+                Write-Output "Attempting upgrade of Perch Log Shipper"
+            }
+            else {
+                Write-Output "Installing Perch log shipper"
+            }
+
             $source = "https://cdn.perchsecurity.com/downloads/perch-log-shipper-latest.exe"
             $destination = "C:\packages$random\perch-log-shipper-latest.exe"
             Invoke-WebRequest -Uri $source -OutFile $destination
@@ -81,6 +144,13 @@ function Win_PerchLogShipper_Install {
                 Write-Output "Install error code: $code."
                 Exit 1
             }
+
+            if ($Upgrade) {
+                Write-Output "Perch log shipper upgraded"
+            }
+            else {
+                Write-Output "Perch log shipper installed"
+            }
         }
         Catch {
             $exception = $_.Exception
@@ -94,6 +164,10 @@ function Win_PerchLogShipper_Install {
             Remove-Item -Path "C:\packages$random" -Recurse -Force
         }
 
+        if ($error) {
+            Exit 1
+        }
+
         Exit 0
     }
 }
@@ -101,10 +175,18 @@ function Win_PerchLogShipper_Install {
 if (-not(Get-Command 'Win_PerchLogShipper_Install' -errorAction SilentlyContinue)) {
     . $MyInvocation.MyCommand.Path
 }
- 
-$scriptArgs = @{
-    Token     = $Token
-    Uninstall = $Uninstall
+
+$scriptArgs = @{}
+if ($Token) {
+    $scriptArgs = @{
+        Token = $Token
+        LatestVersion = $LatestVersion
+    }
+}
+if ($Uninstall) {
+    $scriptArgs = @{
+        Uninstall = $Uninstall
+    }
 }
  
 Win_PerchLogShipper_Install @scriptArgs
