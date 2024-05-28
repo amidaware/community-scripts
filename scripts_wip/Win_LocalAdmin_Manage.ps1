054a21475b162d3e9fd9dd9a5396656998e85798
redanthrax <redanthraxx@gmail.com>
2024-05-28 12:41:54 -0700

    fixed issue with account existing

--- scripts_wip/Win_LocalAdmin_Manage.ps1
+++ scripts_wip/Win_LocalAdmin_Manage.ps1
@@ -9,7 +9,7 @@
 .EXAMPLE
     Win_LocalAdmin_Manage -LocalAdminUser CompanyAdmin -LocalPassword Password123
 .EXAMPLE
-    Win_LocalAdmin_Manage -LocalAdminUser CompanyAdmin -LocalPassword Password123 -Enforce
+    Win_LocalAdmin_Manage -LocalAdminUser CompanyAdmin -LocalPassword Password124 -Enforce
 .INSTRUCTIONS
     1. In Tactical RMM, Go to Settings >> Global Settings >> Custom Fields and under Clients, 
     create the following custom fields:
@@ -63,11 +63,25 @@ function Win_LocalAdmin_Manage {
                 Write-Output "$LocalAdminUser exists."
                 if ($Enforce) {
                     Write-Output "Removing all other admins from Administrator Group."
+                    $adminMembers = $adminMembers | Where-Object { -Not([string]::IsNullOrWhiteSpace($_)) }
                     foreach ($adminMember in $adminMembers) {
-                        if (-Not($adminMember -Match $LocalAdminUser) -and -Not([string]::IsNullOrWhiteSpace($adminMember))) {
-                            if(Get-LocalUser | Where-Object { $_.Name -eq $adminMember }) {
-                                Add-LocalGroupMember -Group "Users" -Member $adminMember
-                                Remove-LocalGroupMember -Group Administrators -Member $adminMember
+                        if (-Not($adminMember -Match $LocalAdminUser)) {
+                            if (Get-LocalUser | Where-Object { $_.Name -eq $adminMember }) {
+                                if($adminMember -ne "Administrator") {
+                                    Add-LocalGroupMember -Group "Users" -Member $adminMember
+                                    Remove-LocalGroupMember -Group Administrators -Member $adminMember
+                                }
+                            }
+                        }
+                        else {
+                            #ensure admin is the sid 500 account
+                            Write-Output "Checking $LocalAdminUser account."
+                            $matchAccount = Get-LocalUser | Where-Object { $_.Name -eq $adminMember }
+                            if (-Not($matchAccount.SID -like "*-500")) {
+                                #account exists but is not admin - remove
+                                Write-Output "Removing $($matchAccount.Name) non-sid 500 account."
+                                Remove-LocalUser $matchAccount
+                                Exit 0
                             }
                         }
                     }
