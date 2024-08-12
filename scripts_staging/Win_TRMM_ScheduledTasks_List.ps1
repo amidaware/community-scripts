9ff865aaac32206067b4425143df6501941b1e5b
silversword411 <github.com@datmail.com>
2024-08-12 12:50:41 -0400

    chore: Refactor Win_TRMM_ScheduledTasks_List.ps1 to improve task information retrieval

--- scripts_staging/Win_TRMM_ScheduledTasks_List.ps1
+++ scripts_staging/Win_TRMM_ScheduledTasks_List.ps1
@@ -1,4 +1,22 @@
-# List TRMM Scheduled tasks. Should match info in Tasks tab of TRMM admin
+<#
 
-(Get-ScheduledTask | Where-Object { $_.TaskName -like "Tac*" }).Count
-Get-ScheduledTask -TaskName "Tac*" | Select-Object TaskName, Date | Format-table
+.NOTES
+    v1.2 8/2/2024 silversword411 adding is running column, fixed last run column
+#>
+
+# Get the count of tasks starting with "Tac"
+$taskCount = (Get-ScheduledTask | Where-Object { $_.TaskName -like "Tac*" }).Count
+
+# Output the total count
+Write-Output "Total: $taskCount"
+
+# Get detailed information for tasks starting with "Tac"
+Get-ScheduledTask | Where-Object { $_.TaskName -like "Tac*" } | ForEach-Object {
+    $taskInfo = Get-ScheduledTaskInfo -TaskName $_.TaskName
+    [PSCustomObject]@{
+        TaskName     = $_.TaskName
+        CreationDate = $_.Date
+        LastRunTime  = $taskInfo.LastRunTime
+        IsRunning    = if ($_.State -eq 'Running') { 'Yes' } else { 'No' }
+    }
+} | Format-Table -AutoSize
