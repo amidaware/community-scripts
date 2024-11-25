9be69b5a5d6c2500096cb85f09ba841715771444
dinger1986 <daniel.lamb@flonix.co.uk>
2024-11-25 17:49:02 +0000

    Update Win_Bluescreen_Report.ps1

--- scripts/Win_Bluescreen_Report.ps1
+++ scripts/Win_Bluescreen_Report.ps1
@@ -2,32 +2,43 @@
 .Synopsis
     Bluescreen - Reports bluescreens
 .DESCRIPTION
-    This will check for Bluescreen events on your system. If parameter provided, goes back that number of days
+    This script checks for Bluescreen events on your system. If a parameter is provided, it goes back that number of days to check.
 .EXAMPLE
     365
 .NOTES
     v1 bbrendon 2/2021
-    v1.1 silversword updating with parameters 11/2021    
+    v1.1 silversword updating with parameters 11/2021
+    v1.2 dinger1986 Updated for improved filtering and structure 11/2024
 #>
 
+# Get the parameter (number of days to go back)
+$DaysBack = $args[0]
 
-$param1 = $args[0]
+# Set error handling preference
+$ErrorActionPreference = 'SilentlyContinue'
 
-$ErrorActionPreference = 'silentlycontinue'
+# Determine the time range based on the parameter
 if ($Args.Count -eq 0) {
-    $TimeSpan = (Get-Date) - (New-TimeSpan -Day 1)
-}
-else {
-    $TimeSpan = (Get-Date) - (New-TimeSpan -Day $param1)
+    $StartTime = (Get-Date).AddDays(-1)
+} else {
+    $StartTime = (Get-Date).AddDays(-[int]$DaysBack)
 }
 
+# Retrieve Bluescreen events
+$BlueScreenEvents = Get-WinEvent -FilterHashtable @{
+    LogName      = 'Application';
+    ID           = 1001;
+    ProviderName = 'Windows Error Reporting';
+    Level        = 4;
+    StartTime    = $StartTime
+} | Where-Object { $_.Message -like "*BlueScreen*" }
 
-if (Get-WinEvent -FilterHashtable @{LogName = 'application'; ID = '1001'; ProviderName = 'Windows Error Reporting'; Level = 4; Data = 'BlueScreen'; StartTime = $TimeSpan }) {
-    Write-Output "There has been bluescreen events detected on your system"
-    Get-WinEvent -FilterHashtable @{LogName = 'application'; ID = '1001'; ProviderName = 'Windows Error Reporting'; Level = 4; Data = 'BlueScreen'; StartTime = $TimeSpan }
+# Check and output results
+if ($BlueScreenEvents) {
+    Write-Output "There have been Bluescreen events detected on your system:"
+    $BlueScreenEvents | Format-List TimeCreated, Id, LevelDisplayName, Message
     exit 1
 } else {
-    Write-Output "No bluescreen events detected in the past 24 hours."
+    Write-Output "No Bluescreen events detected in the past $((Get-Date) - $StartTime).Days days."
     exit 0
 }
-
