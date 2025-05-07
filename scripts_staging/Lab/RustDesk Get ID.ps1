398adb0f940e0986e78100c446d16d821d62f8f9
P6g9YHK6 <17877371+P6g9YHK6@users.noreply.github.com>
2025-05-07 13:50:15 +0000

    Update file: RustDesk Get ID.ps1

--- scripts_staging/Lab/RustDesk Get ID.ps1
+++ scripts_staging/Lab/RustDesk Get ID.ps1
@@ -8,33 +8,3 @@ cd $env:ProgramFiles\RustDesk\
 .\RustDesk.exe --get-id | out-host
 
 exit
-
-#V2
-$ErrorActionPreference = 'SilentlyContinue'
-$maxAttempts = 40
-$attempt = 0
-
-# Change directory to RustDesk install folder
-Set-Location "$env:ProgramFiles\RustDesk"
-
-while ($attempt -lt $maxAttempts) {
-    $output = .\RustDesk.exe --get-id
-
-    if ($output -and $output.Trim() -ne "") {
-        Write-Host "Public ID obtained: $output"
-        break
-    } else {
-        Write-Host "Attempt $($attempt + 1): No ID received, retrying in 30 seconds..."
-        Start-Sleep -Seconds 30
-        $attempt++
-    }
-}
-
-if ($attempt -eq $maxAttempts) {
-    Write-Host "Failed to get RustDesk ID after 20 minutes."
-    exit 1
-}
-
-Write-Host "$output"
-
-exit
