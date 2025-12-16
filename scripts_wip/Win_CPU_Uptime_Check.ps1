bee5c320d8544822dfdfd3223b2c05e1b0f263ae
Owen Conti <sid351@gmail.com>
2025-12-16 12:32:46 +0000

    Updated comment help

--- scripts_wip/Win_CPU_Uptime_Check.ps1
+++ scripts_wip/Win_CPU_Uptime_Check.ps1
@@ -5,9 +5,6 @@
     This was written specifically for use as a "Script Check" in mind, where it the output is deliberaly light unless a warning or error condition is found that needs more investigation.
 
     If the totalhours of uptime of the computer is greater than or equal to the warning limit, an error is returned.
-    
-.NOTES
-    Learing taken from "Win_Disk_SMART2.ps1" by nullzilla, and modified by: redanthrax
 #>
 
 [cmdletbinding()]
